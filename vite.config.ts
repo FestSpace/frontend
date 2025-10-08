3ac13c9eb276df20769a77e9d4790134a7a9599a
Gustavo Andriani <gustavo.joseandriani@gmail.com>
2025-10-08 01:39:10 -0300

    commit corretivo

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,5 @@ export default defineConfig({
   plugins: [react(),],
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true
-      }
-    }
   }
 })
