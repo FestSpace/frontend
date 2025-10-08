6cf262086bd83a023ba3fbfebf3555d45582a05a
Gustavo Andriani <gustavo.joseandriani@gmail.com>
2025-10-08 01:35:17 -0300

    atualização de rota

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,7 +10,7 @@ interface ImportMeta {
   env: ImportMetaEnv;
 }
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+const API_URL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
   baseURL: API_URL,
